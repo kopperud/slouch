9f48fdaac5af71506f4ba563e5c9d7c394b8a1e3
Bjorn Tore Kopperud <bjorntko@student.matnat.uio.no>
2016-12-08 23:52:59 +0100

    revert cm2 test to take into account tja != tia when non-ultrametric tree

--- tests/testthat/test_cm2.R
+++ tests/testthat/test_cm2.R
@@ -9,6 +9,7 @@ foo <- function(m){
 N <- 30
 a <- 0.56
 tia <- foo(matrix(abs(rnorm(N*N)), ncol=N))
+tja <- foo(matrix(abs(rnorm(N*N)), ncol=N))
 ta <- foo(matrix(abs(rnorm(N*N)), ncol=N))
 #T.term <- matrix(rep(1, N), ncol=1)
 T.term <- matrix(rnorm(N), ncol=1)
@@ -28,12 +29,12 @@ for(p in 1:N)
 {
   for(q in 1:N)
   {
-    cm2[p,q]<-(((1-exp(-a*T.term[p]))/(a*T.term[p]))*((1-exp(-a*T.term[q]))/(a*T.term[q]))-(exp(-a*tia[p, q])*(1-exp(-a*T.term[q]))/ (a*T.term[q])+ exp(-a*tia[p, q])*(1-exp(-a*T.term[p]))/(a*T.term[p]))*(num.prob[p,q]));
+    cm2[p,q]<-(((1-exp(-a*T.term[p]))/(a*T.term[p]))*((1-exp(-a*T.term[q]))/(a*T.term[q]))-(exp(-a*tia[p, q])*(1-exp(-a*T.term[q]))/ (a*T.term[q])+ exp(-a*tja[p, q])*(1-exp(-a*T.term[p]))/(a*T.term[p]))*(num.prob[p,q]));
   }
 }
 
 ## New version
-cm2_vectorized = calc.cm2(a, T.term, N, tia, ta)
+cm2_vectorized = calc.cm2(a, T.term, N, tia, tja, ta)
 
 test_that("residual var-cov matrix is identical in vectorized form", {
   expect_equal(cm2, cm2_vectorized)
